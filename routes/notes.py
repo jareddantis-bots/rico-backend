96e38190657866c1852f497f617b301f2e6ac78a
Jared Dantis <jareddantis@gmail.com>
2022-08-08 20:50:37 +0800

    routes/notes: Add DELETE method handler

--- routes/notes.py
+++ routes/notes.py
@@ -3,5 +3,6 @@ from flask import Blueprint
 
 
 bp_notes = Blueprint('notes', __name__)
-bp_notes.route('/create', methods=['POST'])(create_note)
-bp_notes.route('/list', methods=['GET'])(get_notes)
+bp_notes.route('/', methods=['POST'])(create_note)
+bp_notes.route('/', methods=['GET'])(get_notes)
+bp_notes.route('/', methods=['DELETE'])(delete_note)
