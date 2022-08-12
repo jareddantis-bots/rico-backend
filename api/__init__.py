eb3c645c06142664740d70d64d357decf99af800
Jared Dantis <jareddantis@gmail.com>
2022-08-12 14:45:54 +0800

    api: Get user ID using auth token

--- api/__init__.py
+++ api/__init__.py
@@ -3,8 +3,10 @@ from models.session import Session
 
 
 def get_user_id() -> int | None:
-    # Get session ID from cookie
-    session_id = request.cookies.get('session_id')
+    # Get session ID from auth header
+    auth_method, session_id = request.headers.get('Authorization', '').split(' ')
+    if auth_method != 'Bearer' or not session_id:
+        return None
 
     # Get session
     session = Session.query.filter_by(id=session_id).first()
