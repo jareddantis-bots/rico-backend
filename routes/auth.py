d33d15426f861e7d45d2feb0a068a4f0f0aab582
Jared Dantis <jareddantis@gmail.com>
2022-08-12 13:45:23 +0800

    routes/auth: Use POST method for callback

--- routes/auth.py
+++ routes/auth.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 
 bp_auth = Blueprint('auth', __name__)
 bp_auth.route('/auth/login', methods=['GET'])(discord_oauth2_begin)
-bp_auth.route('/auth/callback', methods=['GET'])(discord_oauth2_callback)
+bp_auth.route('/auth/callback', methods=['POST'])(discord_oauth2_callback)
