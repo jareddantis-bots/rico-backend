f5db4406dffab582b0e2724fe861a79b8ec9ba47
Jared Dantis <jareddantis@gmail.com>
2022-08-13 21:08:15 +0800

    main: Create Nextcord IPC client

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database import create_app, db
 from flask_cors import CORS
 from middleware.logger import LoggingMiddleware
+from nextcord.ext import ipc
 from requests import session
 from routes import install_routes
 from werkzeug.security import generate_password_hash
@@ -21,6 +22,12 @@ try:
     db_name = config['db']['database']
     admin_user = config['app']['admin']['user']
     admin_pass = generate_password_hash(config['app']['admin']['password'])
+    discord_callback_uri = config['discord']['callback_uri']
+    discord_client_id = config['discord']['client_id']
+    discord_client_secret = config['discord']['client_secret']
+    discord_ipc_host = config['discord']['ipc']['host']
+    discord_ipc_port = config['discord']['ipc']['port']
+    discord_ipc_secret = config['discord']['ipc']['secret']
 except FileNotFoundError:
     raise RuntimeError('config.yml does not exist')
 except KeyError as e:
@@ -41,13 +48,18 @@ app.config['ADMIN_PASS'] = admin_pass
 
 
 # Store Discord OAuth2 details
-app.config['DISCORD_CALLBACK_URI'] = config['discord']['callback_uri']
-app.config['DISCORD_CLIENT_ID'] = config['discord']['client_id']
-app.config['DISCORD_CLIENT_SECRET'] = config['discord']['client_secret']
+app.config['DISCORD_CALLBACK_URI'] = discord_callback_uri
+app.config['DISCORD_CLIENT_ID'] = discord_client_id
+app.config['DISCORD_CLIENT_SECRET'] = discord_client_secret
 
 
-# Create Discord API session
+# Create Discord API session and bot websocket client
 app.config['DISCORD_SESSION'] = session()
+app.config['BOT_IPC'] = ipc.client.Client(
+    host=discord_ipc_host,
+    port=discord_ipc_port,
+    secret_key=discord_ipc_secret
+)
 
 
 # Enable CORS
